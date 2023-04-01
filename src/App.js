bc6f948d8f9777842985a3e1540e54a944f18c95
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-04-02 03:18:54 +0530

    fetching data from api and displaying data in app.js component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  console.log(users, "========>");
+
+  const fetchUserData = () => {
+    fetch(
+      "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=50bbb41b&app_key=09139394cfd11db09e01e4155f2c0cc8&imageSize=REGULAR&field=image"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data.hits);
+      });
+  };
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        <div className="container">
+          {users.map((user) => (
+            <div className="i">
+              <img src={user.recipe.image} />
+            </div>
+          ))}
+        </div>
+      }
     </div>
   );
-}
+};
 
 export default App;
